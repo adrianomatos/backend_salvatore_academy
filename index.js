4d993aade079dd7cc9186fe4e38ffcfa5f12f431
Adriano Matos <adrianotaua@hotmail.com>
2024-05-12 19:24:22 -0300

    Endpoint Read All  GET /personagens criada

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get("/", function (req, res) {
   res.send("Hello World Nodemon!");
 });
 
+// LISTA
+const lista = ["Java", "Android", "Kotlin"];
+
+// ENDPOINT Read All
+app.get("/personagens", function (req, res) {
+  res.send(lista);
+});
+
 // SERVIDOR  OUVINDO
 app.listen(3000, () =>
   console.log("Servidor rodando em http://localhost:3000")

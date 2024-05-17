5782f87d31c397babb971a3bb70709660c87475d
Adriano Matos <adrianotaua@hotmail.com>
2024-05-17 19:47:14 -0300

    Checagem de ítem existente retornando 404

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", function (req, res) {
 });
 
 // LISTA
-const lista = ["Java", "Android", "Kotlin"];
+const lista = ["Java", "Android", "Kotlin", "JavaScript"];
 
 // ENDPOINT Read All
 app.get("/personagens", function (req, res) {
@@ -18,7 +18,11 @@ app.get("/personagens", function (req, res) {
 // ENDPOINT Read By ID
 app.get("/personagens/:id", function (req, res) {
   const id = req.params.id;
-  res.send(lista[id - 1]);
+  const item = lista[id - 1];
+  if (!item) {
+    return res.status(404).send("ALERTA: Ítem não encontrado");
+  }
+  res.send(item);
 });
 
 // Sinalisando que express está usando JSON
@@ -29,10 +33,10 @@ app.post("/personagens", function (req, res) {
   const body = req.body;
   const novoItem = body.nome;
   if (!novoItem) {
-    return res.status(400).send("ALERTA: FALTA PROPRIEDADE NOME");
+    return res.status(400).send("ALERTA: Falta propriedade NOME");
   }
   if (lista.includes(novoItem)) {
-    return res.status(409).send("ALERTA: ÍTEM JÁ EXISTE");
+    return res.status(409).send("ALERTA: Ítem JÁ EXISTE");
   }
   lista.push(novoItem);
   res.status(201).send("Ítem adicionado com sucesso: " + novoItem);
@@ -41,13 +45,16 @@ app.post("/personagens", function (req, res) {
 // ENDPOINT Update PUT /personagens/id
 app.put("/personagens/:id", function (req, res) {
   const id = req.params.id; // Acessando o parâmetro ID da rota
+  if (!lista[id - 1]) {
+    return res.status(404).send("ALERTA: Ítem não encontrado");
+  }
   const body = req.body; // Acessando o BODY da requisição
   const novoItem = body.nome; // Acessando propriedade nome do body
   if (!novoItem) {
-    return res.status(400).send("ALERTA: FALTA PROPRIEDADE NOME");
+    return res.status(400).send("ALERTA: Falta propriedade NOME");
   }
   if (lista.includes(novoItem)) {
-    return res.status(409).send("ALERTA: ÍTEM JÁ EXISTE");
+    return res.status(409).send("ALERTA: Ítem JÁ EXISTE");
   }
   lista[id - 1] = novoItem; // Atualisamos na lista pelo ID - 1
   res.send("Ítem alterado com sucesso: " + id + " - " + novoItem);
@@ -56,6 +63,9 @@ app.put("/personagens/:id", function (req, res) {
 // ENDPOINT DELETE /personagens/id
 app.delete("/personagens/:id", function (req, res) {
   const id = req.params.id; // Acessando o parâmetro ID da rota
+  if (!lista[id - 1]) {
+    return res.status(404).send("ALERTA: Ítem não encontrado");
+  }
   delete lista[id - 1]; // Deleta pelo ID - 1
   res.send("Ítem removido com sucesso: " + id);
 });
